ce752a61ed61f6911ad5ee734fbc856b000cac8a
asavy79 <asavard@uoguelph.ca>
2025-10-31 13:07:29 -0600

    started functionality for updating user positions and account balances

--- engine_server/db_functions.py
+++ engine_server/db_functions.py
@@ -1,30 +1,47 @@
-from engine_server.db_session import SessionFactory
-from models import Order, Trade
+
+from models import Order, Trade, Account, Position
 from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select, delete
 
 
-async def add_db_order(order: Order):
-
-    async with SessionFactory() as session:
-        try:
-            async with session.begin():
-                session.add(order)
-                await session.flush()
-                await session.refresh(order)
-            return order
-        except SQLAlchemyError:
-            await session.rollback()
-            raise
+async def add_db_order(order: Order, session: AsyncSession):
+    session.add(order)
+    await session.flush()
+    await session.refresh(order)
+    return order
 
 
-async def add_trade(trade: Trade):
-    async with SessionFactory() as session:
-        try:
-            async with session.begin():
-                session.add(trade)
-                await session.flush()
-                await session.refresh(trade)
-            return trade
-        except SQLAlchemyError:
-            await session.rollback()
-            raise
+async def handle_trade(trade: Trade, session: AsyncSession):
+    session.add(trade)
+
+    await update_account_balances(trade, session)
+    await update_positions(trade, session)
+
+
+async def update_account_balances(trade: Trade, session: AsyncSession):
+    buyer_account: Account = trade.buy_order.account
+    seller_account: Account = trade.sell_order.account
+
+    buyer_account.balance -= trade.price
+    seller_account.balance += trade.price
+
+
+async def update_positions(trade: Trade, session: AsyncSession):
+    buyer_account: Account = trade.buy_order.account
+    seller_account: Account = trade.sell_order.account
+
+    seller_pos_result = await session.execute(select(Position).where(Position.account_id == seller_account.id).where(Position.symbol == trade.symbol))
+    seller_position = seller_pos_result.scalar_one_or_none()
+
+    buyer_pos_result = await session.execute(select(Position).where(Position.account_id == buyer_account.id).where(Position.symbol == trade.symbol))
+    buyer_position = seller_pos_result.scalar_one_or_none()
+
+    if not buyer_position:
+        new_position = Position()
+
+    if seller_position:
+        seller_position.quantity -= trade.quantity
+
+        if seller_position.quantity <= 0.0:
+            await session.execute(delete(Position).where(Position.id == seller_position.id))
